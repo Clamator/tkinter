2772d1b652bde7872839e804f1e4cc9d352e0187
Clamator <kalmah23@gmail.com>
2022-02-01 19:13:04 +0300

    testing Table in tkinter - before making the changes in main_wallet

--- 8_pathlib.py
+++ 8_pathlib.py
@@ -4,12 +4,13 @@ import csv
 from pathlib import *
 
 win = tk.Tk()
+win.geometry('800x400+600+300')
 columns=('operation', 'how_much', 'comment', 'date')
 
 treeview = ttk.Treeview(win, show="headings", columns=columns, selectmode="browse")
-treeview.column('operation', width=150,anchor='center')
-treeview.column('how_much', width=150,anchor='center')
-treeview.column('comment', width=150,anchor='center')
+treeview.column('operation', width=100,anchor='center')
+treeview.column('how_much', width=100,anchor='center')
+treeview.column('comment', width=250,anchor='center')
 treeview.column('date', width=150,anchor='center')
 
 treeview.heading('operation', text='operation')
@@ -18,13 +19,27 @@ treeview.heading('comment', text='comment')
 treeview.heading('date', text='date')
 
 treeview.pack(side='left', fill='both', expand=tk.YES)
-operation = ['Computer1', 'Server', 'Notebook']
-how_much = ['10.13.71.223','10.25.61.186','10.25.11.163']
-comment = ['1','2','3',]
-date = ['5','5','5']
+#operation = ['Computer1', 'Server', 'Notebook']
+#how_much = ['10.13.71.223','10.25.61.186','10.25.11.163']
+#comment = ['1','2','3',]
+#date = ['5','5','5']
 
+cur_path = Path.cwd()
+com_his = cur_path/'wallet_project'/'categories'/'common_history.csv'
+with open(com_his, mode='r') as file:
+    order = ('operation', 'how much', 'comment', 'date')
+    reader = csv.DictReader(file, fieldnames=order)
+    main_lst = []
+    for row in reader:
+        main_lst.append(row)
 
-for i in range(min(len(operation), len(how_much))):
-    treeview.insert('', tk.END, values=(operation[i], how_much[i], comment[i], date[i]))
+    #for el in reversed(main_lst):
+    #    print(el['operation'], el['how much'],el['comment'],el['date'])
+
+for el in(main_lst):
+    treeview.insert('', tk.END, values=(el['operation'], el['how much'],el['comment'],el['date']))
 
 win.mainloop()
+
+# treeview.insert('', tk.END, values=(operation[i], how_much[i], comment[i], date[i]))
+# dict_values(['refill', '1000.0', 'salary', '2022-01-20 12:43:35.324424'])
